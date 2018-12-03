4337e8138cd3fb3b728de0163558c962c61234c7
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2018-12-03 23:16:23 +0100

    day20181203 - Part Two Solution

--- src/app/day20181203/day20181203.component.ts
+++ src/app/day20181203/day20181203.component.ts
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Day20181203Component implements OnInit {
 
-  sPartOneData: string;
+  sAdventDayData: string;
+  sPartTwoData: string;
   aLines: string[];
   sPartOneAnswer: number;
-  aCounts: number[];
+  sPartTwoAnswer: number;
   aMutual: number[][];
 
   constructor() {
@@ -23,7 +24,7 @@ export class Day20181203Component implements OnInit {
   public runPart1() {
     this.sPartOneAnswer = 0;
 
-    this.aLines = this.sPartOneData.split('\n');
+    this.aLines = this.sAdventDayData.split('\n');
 
     // check claim each with everyone
     for (let iFirst = 0; iFirst < this.aLines.length; iFirst++) {
@@ -60,6 +61,46 @@ export class Day20181203Component implements OnInit {
 
   }
 
+  public runPart2() {
+    this.sPartTwoAnswer = 0;
+
+    this.aLines = this.sAdventDayData.split('\n');
+
+    let iCorrectId = -1;
+    let bIsWrong;
+    // check claim each with everyone
+    for (let iFirst = 0; iFirst < this.aLines.length; iFirst++) {
+      const firstClaim = new Claim(this.aLines[iFirst]);
+      if (iCorrectId < 0) {
+        bIsWrong = false; // if we don't have correct yet, for start trust the pretender
+      }
+
+
+      for (let iSecond = 0; iSecond < this.aLines.length; iSecond++) {
+        if (iSecond !== iFirst) {
+        const secondClaim = new Claim(this.aLines[iSecond]);
+
+        // check if we have this same rows and cols number
+        const mutualRows = firstClaim.getUsedRows().filter(v => secondClaim.getUsedRows().includes(v));
+        const mutualCols = firstClaim.getUsedCols().filter(v => secondClaim.getUsedCols().includes(v));
+
+        if (mutualRows.length && mutualCols.length) {
+          bIsWrong = true; // if we fund conflict set that this is wrong
+        }
+      }
+
+    }
+    if (!bIsWrong) {
+      iCorrectId = iFirst; // we got corret, without issue
+      bIsWrong = true; // next claims must be wrong, it was a rules of this task
+    }
+  }
+
+    this.sPartTwoAnswer = new Claim(this.aLines[iCorrectId]).getId();
+
+
+  }
+
 
   ngOnInit() {
   }
@@ -71,15 +112,17 @@ class Claim {
 
   aRowsUsed: number[];
   aColsUsed: number[];
+  iId: number;
 
 
   constructor(private sText: string) {
 
     this.aRowsUsed = [];
     this.aColsUsed = [];
+    this.iId = -1;
 
     if (sText.length > 0) {
-      sText = sText.replace(':', ''); // fix input text
+      sText = sText.replace(':', '').replace('#', ''); // fix input text
       const aTmp = sText.split(' ');
       const aPosStart = aTmp[2].split(',');
       const aLenght = aTmp[3].split('x');
@@ -89,6 +132,7 @@ class Claim {
       const nRowLenght = (Number)(aLenght[0]);
       const nColLenght = (Number)(aLenght[1]);
 
+      this.iId = (Number)(aTmp[0]);
       this.aRowsUsed = Array.from(new Array(nRowLenght), (x, i) => nRowStart + i); // fill used rows number
       this.aColsUsed = Array.from(new Array(nColLenght), (x, i) => nColStart + i); // fill used cols number
     }
@@ -100,6 +144,10 @@ class Claim {
 
   public getUsedCols() {
     return this.aColsUsed;
+  }
+
+  public getId(): number {
+    return this.iId;
   }
 
 }
