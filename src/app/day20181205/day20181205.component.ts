f2c231c6a4f233867bd11260536c656d4b4a3131
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2018-12-05 22:38:13 +0100

    day20181205 - Part Two Solution

--- src/app/day20181205/day20181205.component.ts
+++ src/app/day20181205/day20181205.component.ts
@@ -9,43 +9,68 @@ export class Day20181205Component implements OnInit {
 
   sAdventDayData: string;
   sPartOneAnswer: number;
+  sPartTwoAnswer: number;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  public runPart1() {
+  runPart1() {
     this.sPartOneAnswer = 0;
-
-    let sTmp: string;
     let sNewText: string = this.sAdventDayData.trim();
 
     // needed data - all chars know by word ;)
     const aChar: string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
+    sNewText = this.reduce(sNewText, aChar); // run global reaction
+
+    this.sPartOneAnswer = sNewText.length ;
+  }
+
+  runPart2() {
+    this.sPartTwoAnswer = 0;
+    let sNewText: string = this.sAdventDayData.trim();
+
+    // needed data - all chars know by word ;)
+    const aChar: string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+    let iBestReduce = sNewText.length;
+      // check all combination
+      aChar.forEach(v => {
+        sNewText = this.sAdventDayData.trim();
+
+        sNewText = sNewText.replace(new RegExp(v, 'g') , ''); // trim actual char
+        sNewText = sNewText.replace(new RegExp(v.toUpperCase(), 'g'), ''); // trim upper actual char
+
+        sNewText = this.reduce(sNewText, aChar); // run global reaction
+        if (sNewText.length < iBestReduce) {
+          iBestReduce = sNewText.length;
+        }
+      });
+    this.sPartTwoAnswer = iBestReduce;
+  }
+
+  private reduce(sNewText, aChar) {
+    let sTmp: string;
     let iLastLenght: number;
     let bIsSomethingToDo = true;
+    // run reacting polymer
     while (bIsSomethingToDo) {
       iLastLenght = sNewText.length;
-
       // trim all combination
       aChar.forEach(v => {
         sTmp = v + v.toUpperCase();
-        sNewText = sNewText.replace(sTmp, ''); // reduce vV
+        sNewText = sNewText.replace(new RegExp(sTmp, 'g'), ''); // reduce vV
         sTmp = v.toUpperCase() + v;
-        sNewText = sNewText.replace(sTmp, ''); // reduce Vv
+        sNewText = sNewText.replace(new RegExp(sTmp, 'g'), ''); // reduce Vv
       });
-
       // if wasn't anny reduce in this, we don't need check next
       if (!(iLastLenght - sNewText.length)) {
         bIsSomethingToDo = false;
       }
     }
-
-    this.sPartOneAnswer = sNewText.length ;
-
-
+    return sNewText;
   }
 
 }
