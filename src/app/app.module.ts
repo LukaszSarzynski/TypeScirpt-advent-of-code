30e497af7376e120274d949e3a3145cd33a81adb
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2018-12-11 01:02:23 +0100

    day20181210 - Solution of Part One&Two

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { Day20181203Component } from './day20181203/day20181203.component';
 import { Day20181204Component } from './day20181204/day20181204.component';
 import { Day20181205Component } from './day20181205/day20181205.component';
 import { Day20181206Component } from './day20181206/day20181206.component';
+import { Day20181207Component } from './day20181207/day20181207.component';
+import { Day20181208Component } from './day20181208/day20181208.component';
+import { Day20181201Component } from './day20181201/day20181201.component';
+import { Day20181210Component } from './day20181210/day20181210.component';
 
 
 @NgModule({
@@ -18,7 +22,11 @@ import { Day20181206Component } from './day20181206/day20181206.component';
     Day20181203Component,
     Day20181204Component,
     Day20181205Component,
-    Day20181206Component
+    Day20181206Component,
+    Day20181207Component,
+    Day20181208Component,
+    Day20181201Component,
+    Day20181210Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/day20181210/day20181210.component.ts
@@ -0,0 +1,158 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'aofc-day20181210',
+  templateUrl: './day20181210.component.html',
+  styleUrls: ['./day20181210.component.css']
+})
+export class Day20181210Component implements OnInit {
+
+  sAdventDayData: string;
+  sPartOneAnswer: number;
+  sAdventDayDatahow: string;
+
+  aPointRules: PointRule[] = [];
+
+  iGridXStart: number;
+  iGridYStart: number;
+  iGridXEnd: number;
+  iGridYEnd: number;
+
+  aGird: number[][] = [];
+
+  constructor() {
+
+
+   }
+
+
+  runPart1() {
+    this.sPartOneAnswer = 0;
+    this.importData();
+
+    let iStep = 0;
+
+    // find vertifical line on 8 size, and don't fund added next step
+    while (!this.issetVerticalLine()) {
+      this.fillGirdOfSteep(++iStep);
+      console.log(iStep);
+      if (iStep > 978000) {
+        break;
+      }
+    }
+
+    this.sPartOneAnswer = iStep;
+    this.sAdventDayDatahow = this.girdToHtml();
+  }
+
+  private issetVerticalLine(iLenght = 8) {
+
+    // if this is still bigg to check, ignor this result
+    if ((this.iGridYEnd - this.iGridYStart) > 20) {
+      return false;
+    }
+
+    let iFundLineLenght;
+    for (let gx: number = this.iGridXStart; gx <= this.iGridXEnd; gx++){
+      iFundLineLenght = 0;
+      for (let gy = this.iGridYStart; gy <= this.iGridYEnd; gy++) {
+        if (typeof this.aGird[gy] === 'undefined') {
+          iFundLineLenght = 0;
+        } else if (typeof this.aGird[gy][gx] === 'undefined') {
+          iFundLineLenght = 0;
+        } else {
+          iFundLineLenght++;
+        }
+
+        if (iFundLineLenght >= iLenght) {
+          // if fund vertical line, probably we got a text to decode
+          return true;
+        }
+      }
+    }
+
+    return false;
+
+  }
+
+  private importData() {
+    const aLines = this.sAdventDayData.split('\n');
+
+    // import x adn y coridation whith it move
+    for (let index = 0; index < aLines.length; index++) {
+      const aLinNum =  aLines[index].match(/\-?\d+/g);
+      if (aLinNum !== null) {
+      this.aPointRules.push({xst: (Number)(aLinNum[0]),
+                             yst: (Number)(aLinNum[1]),
+                             xve: (Number)(aLinNum[2]),
+                             yve: (Number)(aLinNum[3])
+                            });
+                          }
+    }
+
+  }
+
+  private fillGirdOfSteep(iSteep) {
+
+    this.aGird = []; // clear gird
+
+    let aTmpPointsX: number[] = []; // points X for this step
+    let aTmpPointsY: number[] = []; // points Y for this step
+
+    for (const Point of this.aPointRules) {
+      // calcuate point for step
+      const nXofSteep = Point.xst + Point.xve * iSteep;
+      const nYofSteep = Point.yst + Point.yve * iSteep;
+      aTmpPointsX.push(nXofSteep);
+      aTmpPointsY.push(nYofSteep);
+
+      if (typeof this.aGird[nYofSteep] === 'undefined') {
+        this.aGird[nYofSteep] = [];
+      }
+      // fill Gird of this steps
+      this.aGird[nYofSteep][nXofSteep] = 1;
+    }
+
+    //find max adn min
+    this.iGridXStart = Math.min.apply(null, aTmpPointsX);
+    this.iGridYStart = Math.min.apply(null, aTmpPointsY)
+    this.iGridXEnd = Math.max.apply(null, aTmpPointsX);
+    this.iGridYEnd = Math.max.apply(null, aTmpPointsY);
+
+
+  }
+
+  girdToHtml() {
+    let sText = '';
+    let tmpPointValue = '';
+
+
+    for (let gy = this.iGridYStart; gy <= this.iGridYEnd; gy++) {
+      for (let gx: number = this.iGridXStart; gx <= this.iGridXEnd; gx++) {
+        if (typeof this.aGird[gy] === 'undefined') {
+          tmpPointValue = ' ';
+        } else if (typeof this.aGird[gy][gx] === 'undefined') {
+          tmpPointValue = ' ';
+        } else {
+          // if we have any value in Gird array, mark it!
+          tmpPointValue = '#';
+        }
+        sText +=  tmpPointValue;
+      }
+      sText += '<br />';
+    }
+
+    return sText;
+  }
+
+  ngOnInit() {
+  }
+
+}
+
+interface PointRule {
+  xst: number;
+  yst: number;
+  xve: number;
+  yve: number;
+}
