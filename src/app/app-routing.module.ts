618a08429632c983ddd1b6594e4678771e060899
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2019-01-03 22:16:14 +0100

    Deploy to GitHub pages actual result

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Day20181202Component } from './day20181202/day20181202.component';
 import { Day20181214Component } from './day20181214/day20181214.component';
+import { Day20181212Component } from './day20181212/day20181212.component';
 
 export const routes: Routes = [
   {
     path : '2018/02',
     component: Day20181202Component
+  },
+  {
+    path : '2018/12',
+    component: Day20181212Component
   },
   {
     path : '2018/14',
